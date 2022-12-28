4b3b75f7e57df57f3c4c6d65d518606621df756e
Marlar7 <novenhnin@gmail.com>
2022-12-28 21:26:48 +0630

    realtime db

--- src/views/RealTime.vue
+++ src/views/RealTime.vue
@@ -1,13 +1,28 @@
 <template>
+<div>
+<h1>Realtime database</h1>
+    <div v-for="post in posts" :key="post.id">
+        <div> 
+              <h1>{{post.title}}</h1>
+              <p> {{post.body}}</p>
+        </div><hr>
+        </div>
+</div>
     
+
 </template>
 <script>
 import {db} from "../firebase/config"
+import {ref}from "vue"
 export default {
     setup(){
+        let posts=ref([]);
         db.collection("posts").onSnapshot((snap)=>{
-            
+           posts.value= snap.docs.map((doc)=>{
+                return {...doc.data(),id:doc.id}
+            })
         })
+        return {posts};
     }
 }
 </script>
