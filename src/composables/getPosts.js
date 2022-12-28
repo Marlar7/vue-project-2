1f4c1d7a631ce712529d4b4202a392c438e63c8f
Marlar7 <novenhnin@gmail.com>
2022-12-28 11:42:14 +0630

    firebase

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -7,16 +7,12 @@ let getPosts=()=>{
     let error= ref ("");
     let load =async() => {
     try {
-      // await new Promise((resolve,reject)=>{
-        
-      //   setTimeout(resolve,2000)
-      // })
-        let response = await fetch("http://localhost:3000/Posts")
-          if (response.status===404){
-            throw new Error("not found url");
-          }
-          let datas=await response.json();
-          posts.value=datas;
+      
+       let res= await db.collection("posts").get()
+          posts.value= res.docs.map((doc)=>{
+            return {id:doc.id, ...doc.data()}
+          })
+     
     }catch(err){
           error.value=err.message;
     }
